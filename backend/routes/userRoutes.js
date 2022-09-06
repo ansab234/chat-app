b01881c4684f8196cf32abcbcef6943a972f9f59
ansab234 <ansabali067@gmail.com>
2022-09-06 12:38:53 +0500

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {registerUser, authUser, allUsers} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.post("/", registerUser);
+router.post("/login", authUser);
+router.get("/", protect, allUsers);
+
+module.exports = router;
+
