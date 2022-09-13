5e6640d35390d87c3d56025aadb989657b3d105b
ansab234 <ansabali067@gmail.com>
2022-09-13 11:33:40 +0500

    frontend-updated

--- client/src/components/Authentication/Login.js
+++ client/src/components/Authentication/Login.js
@@ -107,17 +107,6 @@ const Login = () => {
       >
         Login
       </Button>
-      <Button
-        variant="solid"
-        colorScheme="red"
-        width="100%"
-        onClick={() => {
-          setEmail("guest@example.com");
-          setPassword("123456");
-        }}
-      >
-        Get Guest User Credentials
-      </Button>
     </VStack>
   );
 };
