d957140a5fc0b202128b6c865575e6998f1f3408
ansab234 <ansabali067@gmail.com>
2022-09-14 14:50:26 +0500

    heroku-deploy

--- server/server.js
+++ server/server.js
@@ -17,6 +17,24 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
