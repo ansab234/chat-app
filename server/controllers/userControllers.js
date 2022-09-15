68c65278481f8f33e4943299f9f43895cf210b78
ansab234 <ansabali067@gmail.com>
2022-09-15 12:51:59 +0500

    backend-chages

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -45,14 +45,12 @@ const authUser = asyncHandler(async (req, res) => {
 
   if (user && (await user.matchPassword(password))) {
     res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      pic: user.pic,
+      message: `${user.name} Successfully Signed In!!`,
       token: generateToken(user._id),
     });
+    console.log(`${user.name} Signed In Successfully!!`.cyan.bold.underline);
   } else {
-    res.status(401);
+    res.status(401).json({ message: "Invalid Email or Password" });
     throw new Error("Invalid Email or Password");
   }
 });
